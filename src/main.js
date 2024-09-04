1e5a67bfca1ba64ef97b23ded408899abd564102
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-03 19:29:11 -0500

    chore: configured dependencies and options for app instance.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,40 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import 'primeicons/primeicons.css';
+import Card from "primevue/card";
+import Button from "primevue/button";
+import SelectButton from "primevue/selectbutton";
+import Drawer from "primevue/drawer";
+import Avatar from "primevue/avatar";
+import Menu from "primevue/menu";
+import Menubar from "primevue/menubar";
+import Toolbar from "primevue/toolbar";
+import i18n from "./i18n.js";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    ripple: true, theme: {
+        preset: Aura, options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
+    }
+});
+
+app.component('pv-card', Card)
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
+    .component('pv-drawer', Drawer)
+    .component('pv-avatar', Avatar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', Menubar)
+    .component('pv-toolbar', Toolbar);
+
+app.use(i18n);
+
+app.mount('#app');
