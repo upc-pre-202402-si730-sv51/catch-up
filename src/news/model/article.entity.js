d5fe570149327e56cfcdd9edd7f81ea4119f91b1
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-03 19:41:34 -0500

    feat(news): added article and source entities.

---
+++ src/news/model/article.entity.js
@@ -0,0 +1,12 @@
+import {Source} from "./source.entity.js";
+
+export class Article {
+    constructor(title = '', description = '', url = '', urlToImage = '', source = null, publishedAt = '') {
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.source = source ? new Source(source) : null;
+        this.publishedAt = new Date(publishedAt);
+    }
+}
